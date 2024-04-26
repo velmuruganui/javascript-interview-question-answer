a456a0fdb0e1e92fe55987c07b86a383df7b1ce2
Velmurugan <velmuruganfrontenddeveloper@gmail.com>
2024-04-26 19:00:03 +0530

    api url fixes

--- crypto-dashboard/custom.js
+++ crypto-dashboard/custom.js
@@ -2,7 +2,7 @@
 const cardContainer = document.querySelector('#card-container')
 
 
-const baseUrl = 'https://api.coingecko.com/api/v3/coins/maarkets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+const baseUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
 
 
