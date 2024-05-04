19d73e140d0908dd466121c0c580230735fc877d
Velmurugan <velmuruganfrontenddeveloper@gmail.com>
2024-05-04 05:42:57 +0530

    Update custom.js

--- create-a-modal/custom.js
+++ create-a-modal/custom.js
@@ -1,57 +1,32 @@
-const button = document.querySelector('#openModalButton');
-const modal = document.querySelector('#myModal');
-const close = document.querySelector('.close');
- 
-const audioElement = new Audio('audio/error.mp3');
 
-function openModal() {
-    modal.style.display = 'block';
-    document.body.classList.add('modal-open');
-    
-    document.addEventListener('keydown', function (event) { 
-        console.log(event);
-           if(event.key === "Escape"){
-             modal.style.display = 'none';
-             document.body.classList.add('modal-open');
-           }else{
-            audioElement.play();
-           }
-    })
+const button = document.querySelector('#opemModalButton')
+const close = document.querySelector(".close")
+const modal = document.querySelector("#modal")
+
+const audioEle = new Audio('audio/error.mp3')
+
+function openModal(){
+  modal.style.display = 'block';
+  document.body.classList.add('modal-open')
+  document.addEventListener('keydown', modalkeyDownListener)
 }
-  
-function closeModal() {
-    modal.style.display = 'none';
-    document.body.classList.remove('modal-open');
+
+function closeModal(){
+  modal.style.display = 'none';
+  document.body.classList.remove('modal-open')
 }
-  
-  // Event listeners
-  button.addEventListener('click', openModal);
-  close.addEventListener('click', closeModal);
-  
-  // Keydown event listener
-  document.addEventListener('keydown', function (event) {
-    if (event.key === "Escape") {
-      closeModal();
-    }
-  });
-
-document.addEventListener('DOMContentLoaded', function () {
-        button.addEventListener('click', function () {
-            modal.style.display = 'block';
-            document.body.classList.add('modal-open');
-        });
-
-        close.addEventListener('click', function () {
-            modal.style.display = 'none';
-            document.body.classList.add('modal-open');
-        });
-});
 
 
 
 
+function modalkeyDownListener(event){
+  if(event.key === "Escape"){
+    closeModal()
+  }else{
+    audioEle.play()
+  }
+}
 
 
-
-
-
+button.addEventListener('click', openModal)
+close.addEventListener('click', closeModal)
